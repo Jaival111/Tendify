f04b0b1ff5f8ab0f867c629e7f82abeeed3cac21
Jaival <jaivalchauhan111@gmail.com>
2025-06-04 15:15:27 +0530

    fixed deploy

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from typing import Annotated
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
@@ -7,23 +8,27 @@ from routers import auth_router, subject_router
 import models
 
 app = FastAPI()
-app.include_router(auth_router.router)
-app.include_router(subject_router.router)
-
-origins = [
-    "https://tendify-frontend.onrender.com",
-    "http://localhost:5173",  # For local development
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "https://tendify-frontend.onrender.com",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["*"],
     expose_headers=["*"],
     max_age=3600,
 )
+
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=["*"]
+)
+
+app.include_router(auth_router.router)
+app.include_router(subject_router.router)
 
 def get_db():
     db = SessionLocal()
